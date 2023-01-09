b995bbc3b44f96cd3d13b39ff605cafc676ea3f0
Dreamin121 <12185744@qq.com>
2023-01-09 15:11:27 +0800

    add type

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello Applets',
+    motto: 'Hello Applet',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
